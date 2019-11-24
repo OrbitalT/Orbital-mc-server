ac88a7421f67606082a3d63ffc6f6cf9491cf3f5
NukeZ <brycelogue@gmail.com>
2019-11-23 20:50:28 -0500

    Almost there

---
+++ index.js
@@ -0,0 +1,90 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+var cmd = require('node-command-line'),
+  Promise = require('bluebird');
+
+// Enable live reload for all the files inside your project directory
+require('electron-reload')(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain
+} = electron;
+
+let mainWindow;
+
+// ready
+app.on('ready', function() {
+  // Create new main window
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+
+    width: 1250,
+    height: 750
+  });
+  //Load html
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+  //quit app full
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
+
+  //adds temp
+  const mainMenu = Menu.buildFromTemplate(mainWindowTemplate);
+
+  Menu.setApplicationMenu(mainMenu);
+});
+
+//installing server files
+ipcMain.on('item:mp3', function(e, item) {
+  Promise.coroutine(function *() {
+    yield cmd.run('start cmd /k cd D:/All code etc/Orbital-mc-server/servers');
+  })();
+});
+
+//menu temp
+const mainWindowTemplate = [{
+  label: 'File',
+  submenu: [{
+    label: 'Quit',
+    accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+    click() {
+      app.quit();
+    }
+  }]
+}];
+
+//mac support
+if (process.platform == "darwin") {
+  mainMenuTemp.unshift({});
+}
+
+//dev tool
+if (process.env.NODE_ENV !== 'production') {
+  mainWindowTemplate.push({
+    label: "Dev Tools",
+    submenu: [{
+        label: 'Toggle Devtools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
